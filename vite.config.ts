54edd30e2eb005e146d61484b9b31f47ba6d106b
Pokeya <pokeya.mystic@gmail.com>
2024-06-20 21:44:53 +0800

    build: inject styles into JS bundle

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { rawMdx } from './plugins/vite-plugin-raw.mdx'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
+import vitePluginCssInjectedByJs from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,7 +21,8 @@ export default defineConfig({
           dest: 'assets/sounds'
         }
       ]
-    })
+    }),
+    vitePluginCssInjectedByJs()
   ],
   resolve: {
     alias: {
@@ -51,6 +53,7 @@ export default defineConfig({
         exports: 'named'
       }
     },
+    cssCodeSplit: true,
     emptyOutDir: false
   }
 })
