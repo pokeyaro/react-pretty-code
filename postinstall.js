a1c5c6daa11f775525c56542bba37492a70ed2c1
Pokeya <pokeya.mystic@gmail.com>
2024-06-21 23:28:51 +0800

    fix path bug

--- postinstall.js
+++ postinstall.js
@@ -2,39 +2,21 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// 获取当前文件名和目录名
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// 日志函数：写入日志到 postinstall.log 文件
-function log(message) {
-  const logFile = path.join(__dirname, 'postinstall.log');
-  fs.appendFileSync(logFile, message + '\n');
-}
-
-// 定义源目录（库中的静态资源目录）
 const srcDir = path.join(__dirname, 'dist/assets');
-log(`Source Directory: ${srcDir}`);
+const projectRoot = path.resolve(__dirname, '../../../../../');
 
-// 写死路径往上三层找到用户项目的根目录
-const projectRoot = path.resolve(__dirname, '../../../../');
-log(`Project Root Directory: ${projectRoot}`);
-
-// 定义目标目录（用户项目的 public/assets 目录）
 const targetDir = path.join(projectRoot, 'public/assets');
-log(`Target Directory: ${targetDir}`);
 
-// 函数：同步地创建目标目录及其所有父目录
 function createDirIfNotExists(dest) {
   if (!fs.existsSync(dest)) {
-    log(`Creating directory: ${dest}`);
     fs.mkdirSync(dest, { recursive: true });
   }
 }
 
-// 函数：同步地复制文件和目录
 function copyFiles(src, dest) {
-  // 检查源目录是否存在
   if (!fs.existsSync(src)) {
     throw new Error(`Source directory does not exist: ${src}`);
   }
@@ -49,16 +31,13 @@ function copyFiles(src, dest) {
     if (entry.isDirectory()) {
       copyFiles(srcPath, destPath);
     } else {
-      log(`Copying file from ${srcPath} to ${destPath}`);
       fs.copyFileSync(srcPath, destPath);
     }
   }
 }
 
-// 执行复制操作，并捕获错误
 try {
   copyFiles(srcDir, targetDir);
-  log('Files copied successfully.');
 } catch (error) {
-  log(`Error copying files: ${error.message}`);
+  console.warn(`Error copying files: ${error.message}`);
 }
