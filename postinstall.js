da98e18d72a92823c8cc62ecb95a15b42f6c53cf
Pokeya <pokeya.mystic@gmail.com>
2024-06-21 22:44:15 +0800

    fix: debugging Code

--- postinstall.js
+++ postinstall.js
@@ -2,15 +2,19 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// 获取当前文件的目录
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// 定义源目录和目标目录
 const srcDir = path.join(__dirname, 'dist/assets');
-const targetDir = path.join(__dirname, '../public/assets');
+const targetDir = path.join(process.cwd(), 'public/assets');
 
+// 日志输出源目录和目标目录
 console.log(`Source Directory: ${srcDir}`);
 console.log(`Target Directory: ${targetDir}`);
 
+// 复制文件函数
 function copyFiles(src, dest) {
   if (!fs.existsSync(dest)) {
     console.log(`Creating target directory: ${dest}`);
@@ -31,6 +35,7 @@ function copyFiles(src, dest) {
   }
 }
 
+// 执行复制操作，并捕获错误
 try {
   copyFiles(srcDir, targetDir);
   console.log('Files copied successfully.');
